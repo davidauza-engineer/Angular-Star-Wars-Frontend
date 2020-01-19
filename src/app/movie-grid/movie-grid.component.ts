9056e1cd1593d8bf7a60c1a809708625dadbb582
enrike82 <38122713+enrike82@users.noreply.github.com>
2020-01-19 15:14:43 -0500

    Crear movie-grid.component.ts

---
+++ src/app/movie-grid/movie-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-grid',
+  templateUrl: './movie-grid.component.html',
+  styleUrls: ['./movie-grid.component.css']
+})
+export class MovieGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
