fa555420dec30b8034fb740f7c14a7ec0de6c5c5
apfarid <apfarid780@gmail.com>
2020-01-19 12:16:03 -0500

    Crear componente main-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
